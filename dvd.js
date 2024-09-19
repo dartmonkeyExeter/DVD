afc59f7df029d6b5e8a5006ef61140d9509f47b6
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2024-09-19 11:46:10 +0100

    i love laufey

--- dvd.js
+++ dvd.js
@@ -9,16 +9,22 @@ document.addEventListener("DOMContentLoaded", () => {
     const body = document.body;
     const path1 = document.getElementById("path1");
     const path2 = document.getElementById("path2");
+
+    var cornerHitAmount = 0;
+    var sideHitAmount = 0;
+
+    var cornerHitText = document.getElementById("cornerHitText")
+    var sideHitText = document.getElementById("sideHitText")
     
     var newColor = colorList[Math.floor(Math.random() * colorList.length)];
     path1.style.fill = newColor;
     path2.style.fill = newColor;
 
     while (xVel === 0) {
-        xVel = (Math.random() * 2) - 1;
+        xVel = Math.floor(Math.random() * 3) - 1;
     }
     while (yVel === 0) {
-        yVel = (Math.random() * 2) - 1;
+        yVel = Math.floor(Math.random() * 3) - 1;
     }
 
     setInterval(() => {
@@ -41,17 +47,33 @@ document.addEventListener("DOMContentLoaded", () => {
             newColor = colorList[Math.floor(Math.random() * colorList.length)];
         }
 
-        var offset = Math.random() * (-0.5 - 0.5) + 0.5;
+        var rand_times = [1.1, 0.9];
+        var offset = rand_times[Math.floor(Math.random() * rand_times.length)];
+
+        var flag1, flag2 = false;
 
         if (curLeft <= 0 || curLeft >= checkRight){
-            xVel = -xVel + offset;
+            xVel = -xVel * offset;
             path1.style.fill = newColor;
             path2.style.fill = newColor;
+            flag1 = true;
+            sideHitAmount += 1;
+            sideHitText.innerHTML = `hits: ${sideHitAmount}`;
         }
-        else if (curTop <= 0 || curTop >= checkBottom){
-            yVel = -yVel + offset;
+        if (curTop <= 0 || curTop >= checkBottom){
+            yVel = -yVel * offset;
             path1.style.fill = newColor;
             path2.style.fill = newColor;
+            flag2 = true;
+            sideHitAmount += 1;
+            sideHitText.innerHTML = `hits: ${sideHitAmount}`;
+        }
+
+        if (flag1 & flag2) {
+            cornerHitAmount += 1;
+            sideHitAmount -= 1;
+            cornerHitText.innerHTML = `corner hits: ${cornerHitAmount}`;
+            sideHitText.innerHTML = `hits: ${sideHitAmount}`;
         }
 
         logoSvg.style.top = (curTop + (yVel * forceMultiplier)) + "px";
